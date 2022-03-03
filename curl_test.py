0c6b92073225b3d0d46d9a07c8df165086bbc6c5
kalaivani <kalaivani.v@corestack.io>
2022-03-03 14:16:00 +0530

    post call

--- curl_test.py
+++ curl_test.py
@@ -1,25 +1,42 @@
 import requests
+import json
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
-url = '<url>'
-myobj = {'<username>': '<password>'}
+url1 = 'http://34.69.63.225:18080/v1/auth/tokens'
+Headers1 = {'Content-Type': 'application/json'}
+data1 = {"username": "admin", "password": "Cl0u4@dmin"}
+#post = requests.post(url1, data=data1, headers=Headers1)
+response = requests.post(url1, headers=Headers1, data=json.dumps(data1)).text
+print(response)
+parsed_json = (json.loads(response))
+Token=parsed_json['data']['token']['key']
+print(Token)
+#url = 'https://dev3.corestack.io/provider/services/executivedashboard/'
+url = 'http://34.69.63.225:18080/v2/cost/dashboard/tenant_cloud_spend?currency=USD&end_date=2022-02-16&start_date=2021-08-01'
+#url = 'http://dev.corestack.io:18080/v1/auth/tokens'
+Headers = {'X-Auth-Token': Token, 'Content-Type': 'application/json', 'X-Auth-User': 'admin'}
 
-x = requests.post(url, data = myobj)
-
+print("Token applied")
+x = requests.get(url, headers=Headers)
+print(x)
+status = x.text
+print(json.dumps(status))
+#data  x.content
+#print(data)
 response = x.status_code
 print(response)
-if response == 200: 
+if response == requests.codes.ok: 
   print("response is 200" )
 else:
   smtpObj = smtplib.SMTP('smtp.office365.com', 587)
   smtpObj.ehlo()
   smtpObj.starttls()
-  smtpObj.login("<sender_mail_id>", "<mail_password>")
-  sender_email = "<sender_mail_id>"
-  receiver_email = "<receiver_mail_id>"
+  smtpObj.login("kalaivani.v@corestack.io", "Cl0ud@123")
+  sender_email = "kalaivani.v@corestack.io"
+  receiver_email = "kalaivani.v@corestack.io"
   msg = MIMEMultipart()
   msg['Subject'] = 'Dev server2 UI Response is not 200'
   msg['From'] = sender_email
