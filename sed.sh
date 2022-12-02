22d23dcf0f015aa2ae5c5be4265e48bf4fbbe016
kalaivairamuthu <kalaivairamuthu@users.noreply.github.com>
2022-12-02 10:32:10 +0530

    Update sed.sh

--- sed.sh
+++ sed.sh
@@ -1,7 +1,2 @@
-while IFS= read -r line; do sed -i "s/10.18.0.18/app.qa.wus.internal.corestack.io/g" $line; done < /home/kalaivani/conf-files
-while IFS= read -r line; do sed -i "s/10.18.0.14/dp.qa.wus.internal.corestack.io/g" $line; done < /home/kalaivani/conf-files 
-while IFS= read -r line; do sed -i "s/10.18.0.4/mongodbprimary.qa.wus.internal.corestack.io/g" $line; done < /home/kalaivani/conf-files 
-while IFS= read -r line; do sed -i "s/10.18.0.7/mongodbsecondary1.qa.wus.internal.corestack.io/g" $line; done < /home/kalaivani/conf-files
-while IFS= read -r line; do sed -i "s/10.18.0.21/mongodbsecondary2.qa.wus.internal.corestack.io/g" $line; done < /home/kalaivani/conf-files
-while IFS= read -r line; do sed -i "s/10.18.0.15/policy.qa.wus.internal.corestack.io/g" $line; done < /home/kalaivani/conf-files
-
+while IFS= read -r line; do sed -i "s/strg/chgstrg/g" $line; done < <file psth to chnage>
+while IFS= read -r line; do sed -i "s/string/chnage_strg/g" $line; done < <conf-files(list of files to change)> 
